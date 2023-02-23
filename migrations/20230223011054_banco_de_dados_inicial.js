b4b5ea2d359becfd140f8d0c935f194f25be6af5
Jennifer <jennifertorchelsen@gmail.com>
2023-02-22 22:54:25 -0300

    migration do banco inicial

---
+++ migrations/20230223011054_banco_de_dados_inicial.js
@@ -0,0 +1,21 @@
+export function up(knex) {
+    return knex.schema
+      .createTable('categoria', function (table) {
+          table.increments('id');
+          table.string('nome', 50).notNullable();
+      })
+      .createTable('produto', function (table) {
+          table.increments('id');
+          table.integer('categoria_id', 10).notNullable();
+          table.string('nome', 50).notNullable();
+          table.string('descricao',50).notNullable();
+          table.decimal('preco', 10,2).notNullable();
+          table.string('foto',255).notNullable();
+      });
+  }
+  
+  export function down(knex) {
+    return knex.schema
+        .dropTable("produto")
+        .dropTable("categoria");
+  }
